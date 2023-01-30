71b47f89be62e91cca7280631146e053c7bd985f
Vinicius Costa Sato <v.alvescosta26@gmail.com>
2023-01-30 12:00:56 -0300

    Cronômetro e relógio

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Item from './Item';
 import styles from'./Lista.module.scss'
 
 export default function Lista(){
@@ -17,10 +18,10 @@ export default function Lista(){
             <h2>Estudos do dia!</h2>
             <ul>
                 {tarefa.map(( item, index ) => (
-                    <li key={index} className={styles.item}>
-                       <h3>{item.tarefa}</h3>
-                       <span>{item.tempo}</span>
-                    </li>
+                   <Item
+                   key={index}
+                    {...item}
+                   />
                 ))}
             </ul>
         </aside>

d8d8c8649f91b8b0d9e35ce5487d4163eefc799e
Vinicius Costa Sato <v.alvescosta26@gmail.com>
2023-01-31 11:19:26 -0300

    Lista dinamica

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,10 +1,11 @@
 import React from'react';
 import styles from './Botao.module.scss'
 
-class Botao extends React.Component <{ children: string}>{
+class Botao extends React.Component <{ children: string, type?: "button" | "submit" | "reset" | undefined }>{
     render() {
+        const { type= "button" } = this.props
         return(
-            <button className={styles.botao}>
+            <button type={type} className={styles.botao}>
                 {this.props.children}
             </button>
         )
